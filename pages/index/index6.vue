34bb14695393e09b9000ef29ab0918d6f10d14c6
yeyanghui <yeyanghui@lixiang.com>
2024-01-18 17:34:25 +0800

    feat: 补间动画

--- pages/index/index6.vue
+++ pages/index/index6.vue
@@ -11,9 +11,6 @@ import { GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader.js";
 import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
 
-// 导入tween
-import * as TWEEN from 'three/examples/jsm/libs/tween.module.js'
-
 onMounted (()=>{
   // 创建场景
   const scene = new THREE.Scene();
@@ -58,10 +55,9 @@ onMounted (()=>{
     requestAnimationFrame(animate);
 
     renderer.render(scene, camera);
-    // 更新tween
-    tween.update()
   };
 
+  animate()
 
   window.addEventListener('resize', ()=>{
     // 重置渲染器宽高比
@@ -102,40 +98,54 @@ onMounted (()=>{
   )
   sphere1.position.x = 4
   scene.add(sphere1)
+  // 创建三个球
+  const sphere2 = new THREE.Mesh(
+      new THREE.SphereGeometry(1,32,32),
+      new THREE.MeshBasicMaterial({
+        color: 0x000ff0
+      })
+  )
+  sphere2.position.x = 0
+  scene.add(sphere2)
+  // 创建三个球
+  const sphere3 = new THREE.Mesh(
+      new THREE.SphereGeometry(1,32,32),
+      new THREE.MeshBasicMaterial({
+        color: 0x0ff0f0
+      })
+  )
+  sphere2.position.x = -4
+  scene.add(sphere3)
 
-  // 创建一个补间对象
-  const tween = new TWEEN.Tween(sphere1.position)
-  tween.to({x:-4}, 1000)
-  tween.onUpdate(()=>{
-    // console.log("%c ---> : 2121","color:#F0F;", sphere1.position.x);
-  })
-  // 循环无数次
-  // tween.repeat(Infinity)
-  // // 循环往复
-  // tween.yoyo(true)
-  // 延迟运行
-  // tween.delay(3000)
-  // 设置缓动函数
-  tween.easing(TWEEN.Easing.Quadratic.InOut)
+  // 创建射线
+  const raycaster = new THREE.Raycaster()
 
+  // 创建鼠标向量，保存鼠标点在了画布的哪个位置上
+  const mouse = new THREE.Vector2()
 
-  const tween2 = new TWEEN.Tween(sphere1.position)
+  window.addEventListener('click',(event)=>{
+    // 转化鼠标向量的x和y
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+    mouse.y = -(event.clientY/ window.innerHeight) * 2 + 1
+    console.log("%c ---> mouse.x, mouse.y: ","color:#F0F;", mouse.x, mouse.y);
 
-  tween2.to({y: -14}, 2000)
+    // 通过摄像机和鼠标位置更新射线
+    raycaster.setFromCamera(mouse, camera)
 
-  tween.chain(tween2)
+    // 计算射线与物体的焦点
+    const intersects = raycaster.intersectObjects([sphere1,sphere2,sphere3])
+    if(intersects.length>0){
+      if(intersects[0].object._isSelected){
+        intersects[0].object.material.color.set(intersects[0].object._originColor)
+        intersects[0].object._isSelected = false
+        return
+      }
 
-  // 启动补间动画
-  tween.start()
-
-  const params = {
-    stop: function(){
-      tween.stop()
+      intersects[0].object._isSelected = true
+      intersects[0].object._originColor = intersects[0].object.material.color.getHex()
+      intersects[0].object.material.color.set(0xff0000)
     }
-  }
-  gui.add(params, 'stop')
-  animate()
-
+  })
 })
 
 
